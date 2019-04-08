28f77ed821f1d9a4bd384e42ca93cd09d796d297
Martin Spicuzza <spicuzza157@gmail.com>
2019-04-08 21:29:35 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,10 +1,23 @@
-def annoying
-    loop do 
-        puts "What's up, Doc?"
-        answer = gets.chomp
-        break if answer == "STOP"
-    end
-    puts "Okay, okay, jeez. I'll stop. Sorry."
+
+def levitation_quiz
+  loop do
+    puts "What is the spell that enacts levitation?"
+    answer = gets.chomp
+    break if answer == "Wingardium Leviosa"
+    puts "You passed the quiz!"
+  end
+	#your code here
 end
+
+
+#def annoying
+#    loop do 
+#        puts "What's up, Doc?"
+#        answer = gets.chomp
+#        break if answer == "STOP"
+#    end
+#    puts "Okay, okay, jeez. I'll stop. Sorry."
+#end
  
-annoying
+#annoying
+
